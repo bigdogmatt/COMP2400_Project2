7ec1fa8708d19075d3dcfa71ba63872f87c02ef5
Matt Kleman <matt.kleman02@gmail.com>
2020-02-04 11:10:59 -0500

    Started file I/O

--- project2.c
+++ project2.c
@@ -1,1 +1,41 @@
 #include <stdio.h>
+
+
+int main()
+{
+	unsigned char message[2048];
+	unsigned char key[2048];
+	int character;
+	int messageLength = 0;
+	int keyLength = 0;
+
+	while( (character = getchar()) != 255 && character != EOF
+	&& messageLength < 2048)
+	{
+		message[messageLength] = character;
+		++messageLength;
+	}
+
+	while (character != 255 && character != EOF)
+	{
+		character = getchar();
+	}
+
+	while( (character = getchar()) != EOF && keyLength < 2048)
+	{
+		key[keyLength] = character;
+		++keyLength;
+	}
+
+	for( int j = 0; j < messageLength; ++j)
+	{
+		char c = message[j];
+		putchar(c);
+	}
+
+	for( int j = 0; j < keyLength; ++j)
+	{
+		char c = key[j];
+		putchar(c);
+	}
+}
